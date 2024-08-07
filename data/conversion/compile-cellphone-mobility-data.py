81c3139639d45530b3a6c67ef8f77043f89a72ea
Tijs Alleman <tijs.alleman@ugent.be>
2024-08-07 11:43:55 -0400

    do the same for the cellphone data

--- data/conversion/compile-cellphone-mobility-data.py
+++ data/conversion/compile-cellphone-mobility-data.py
@@ -119,28 +119,35 @@ merged = pd.merge(data_desired, data_filtered,left_on=['X1', 'Y1'], right_on=['X
 merged = merged[['X1', 'Y1', 'Z2']]
 out = merged.rename(columns={'X1': 'origin', 'Y1': 'destination', 'Z2': 'commuters'})
 
-# step 3: correct for completely new Connecticut FIPS codes 
-## step 3a: compute average demo-weighted fraction of Connecticut population making a trip outside home county
+# step 3: add origin and destination demography
+out['origin_population'] = out['origin'].map(demography.squeeze())
+out['destination_population'] = out['destination'].map(demography.squeeze())
+
+# step 4: correct for completely new Connecticut FIPS codes 
+## step 4a: compute average demo-weighted fraction of Connecticut population making a trip outside home county
 demo_09_old = pd.Series(index=['09001', '09003', '09005', '09007', '09009', '09011', '09013', '09015'], data = [959768, 896854, 185000, 164759, 863700, 268605, 150293, 116418], name='population')
 sum = data.groupby(by='county_o')['pop_flows'].sum()
 sum_offdiagonal = data[data['county_o'] != data['county_d']].groupby(by='county_o')['pop_flows'].sum().reset_index()
 sum_offdiagonal['state_o'] = sum_offdiagonal['county_o'].apply(lambda x: f"{x[0:2]:02}") 
 sum_offdiagonal = sum_offdiagonal[sum_offdiagonal['state_o'] == '09'][['county_o', 'pop_flows']].set_index('county_o').squeeze()
 f_offdiagonal = (((sum_offdiagonal / demo_09_old) * demo_09_old) / demo_09_old.sum()).sum()
-## step 3b: compute average demo-weighted fraction of Connecticut population making a trip inside home county
+## step 4b: compute average demo-weighted fraction of Connecticut population making a trip inside home county
 sum_ondiagonal = data[data['county_o'] == data['county_d']].groupby(by='county_o')['pop_flows'].sum().reset_index()
 sum_ondiagonal['state_o'] = sum_ondiagonal['county_o'].apply(lambda x: f"{x[0:2]:02}") 
 sum_ondiagonal = sum_ondiagonal[sum_ondiagonal['state_o'] == '09'][['county_o', 'pop_flows']].set_index('county_o').squeeze()
 f_ondiagonal = (((sum_ondiagonal / demo_09_old) * demo_09_old) / demo_09_old.sum()).sum() # observation: on/off diagonal estimates are quite consistent between counties
-# step 3c: fill in trips originating from Connecticut to non-Connecticut counties
+# step 4c: fill in trips originating from Connecticut to non-Connecticut counties
 out['state_o'] = out['origin'].apply(lambda x: f"{x[0:2]:02}")          # add origin state code
 out['state_d'] = out['destination'].apply(lambda x: f"{x[0:2]:02}")     # add destination state code
 for fips in out[out['state_o'] == '09']['origin'].unique():             # loop over post-2020 Connecticut codes
-    out.loc[((out['origin'] == fips) & (out['destination'] == fips)), 'commuters'] = (f_ondiagonal * demography.loc[fips]).values # fill in on-diagonal trips
-    out.loc[((out['origin'] == fips) & (out['destination'] != fips)), 'commuters'] = np.ones(len(out.loc[((out['origin'] == fips) & (out['destination'] != fips)), 'commuters'])) * \
-                                                                                        (f_offdiagonal * demography.loc[fips]).values / \
-                                                                                            len(out.loc[((out['origin'] == fips) & (out['destination'] != fips)), 'commuters']) # spread off-diagonal uniformly over all possible destinations  (does not influence outcome of departure-diffusion radiation model)
-# step 3d: fill in all non-Connecticut originating trips to Connecticut counties
+    # fill in on-diagonal trips
+    out.loc[((out['origin'] == fips) & (out['destination'] == fips)), 'commuters'] = (f_ondiagonal * demography.loc[fips]).values
+    # fill in off-diagonal trips: distribute (demo-weighted) over Connecticut state only (uniformly over all spatial patches causes issues in the inference of the dep.-diff. rad. model)
+    n = len(out.loc[((out['origin'] == fips) & (out['destination'] != fips) & (out['state_d'] == '09')), 'commuters'])
+    demo = out.loc[((out['origin'] == fips) & (out['destination'] != fips) & (out['state_d'] == '09')), 'destination_population']
+    demo = demo / demo.sum()
+    out.loc[((out['origin'] == fips) & (out['destination'] != fips) & (out['state_d'] == '09')), 'commuters'] = (f_offdiagonal * demography.loc[fips]).values * demo.values
+# step 4d: fill in all non-Connecticut originating trips to Connecticut counties
 data['state_o'] = data['county_o'].apply(lambda x: f"{x[0:2]:02}") # add origin state code
 data['state_d'] = data['county_d'].apply(lambda x: f"{x[0:2]:02}") # add destination state code
 n = data[((data['state_o'] != '09') & (data['state_d'] == '09'))].groupby(by='county_o')['pop_flows'].sum() # no trip from split '02261' to Connecticut
@@ -148,33 +155,38 @@ for fips in n.index: # long: 15 min
    print(fips)
    out.loc[((out['origin'] == fips) & (out['state_d'] == '09')), 'commuters'] = np.ones(len(out.loc[((out['origin'] == fips) & (out['state_d'] == '09')), 'commuters'])) *  (n.loc[fips] / len(out.loc[((out['origin'] == fips) & (out['state_d'] == '09')), 'commuters']))
 
-# step 4: correct for splitting of '02261' --> '02063' and '02066'
-# step 4a: fill in trips originating from former '02261' 
+# step 5: correct for splitting of '02261' --> '02063' and '02066'
+## step 5a: fill in trips originating from former '02261' 
 pop = pd.Series(index=['02063', '02066'], data=[7102, 2617]) # 2020 census population
 r_pop = pop / pop.sum()
 for fips in ['02063', '02066']:
     # fill in on-diagonal trips
     out.loc[((out['origin'] == fips) & (out['destination'] == fips)), 'commuters'] = (data[((data['county_o'] == '02261') & (data['county_d'] == '02261'))]['pop_flows'] * r_pop.loc[fips]).values
-    # fill in off-diagonal trips
-    out.loc[((out['origin'] == fips) & (out['destination'] != fips)), 'commuters'] = np.ones(len(out.loc[((out['origin'] == fips) & (out['destination'] != fips)), 'commuters'])) * data[((data['county_o'] == '02261') & (data['county_d'] != '02261'))]['pop_flows'].sum() * r_pop.loc[fips] / len(out.loc[((out['origin'] == fips) & (out['destination'] != fips)), 'commuters'])
-# step 4b: distribute all non-'02261' originating trips to former '02261'
+    # fill in off-diagonal trips (demo-weighted & within-Alaska only instead of uniform in order to not create unrealistic trips that cause downstream issues; similar to Connecticut)
+    n = len(out.loc[((out['origin'] == fips) & (out['destination'] != fips) & (out['state_d'] == '02')), 'commuters'])
+    demo = out.loc[((out['origin'] == fips) & (out['destination'] != fips) & (out['state_d'] == '02')), 'destination_population']
+    demo = demo / demo.sum()
+    out.loc[((out['origin'] == fips) & (out['destination'] != fips) & (out['state_d'] == '02')), 'commuters'] = data[((data['county_o'] == '02261') & (data['county_d'] != '02261'))]['pop_flows'].sum() * r_pop.loc[fips] * demo.values 
+## step 5b: distribute all non-'02261' originating trips to former '02261' over two new counties    
 n = data[((data['county_o'] != '02261') & (data['county_d'] == '02261'))].groupby(by='county_o')['pop_flows'].sum() 
 for fips in n.index:
     out.loc[((out['origin'] == fips) & (out['destination'] == '02063')), 'commuters'] = n.loc[fips] * r_pop.loc['02063']
     out.loc[((out['origin'] == fips) & (out['destination'] == '02066')), 'commuters'] = n.loc[fips] * r_pop.loc['02066']
 
-# step 5: add origin and destination demography
-out['origin_population'] = out['origin'].map(demography.squeeze())
-out['destination_population'] = out['destination'].map(demography.squeeze())
+# step 6: remove all trips going into the three smallest US counties (influxes in the dataset are not realistic)
+# Alternative 
+out.loc[((out['origin'] != '48301') & (out['destination'] == '48301')), 'commuters'] = np.nan # --> 113 other counties in the dataset that have an influx into 48301 Loving County (pop: 64) 
+out.loc[((out['origin'] != '15005') & (out['destination'] == '15005')), 'commuters'] = np.nan
+out.loc[((out['origin'] != '48269') & (out['destination'] == '48269')), 'commuters'] = np.nan
 
-# step 6: add the distances (equality of indices asserted)
+# step 7: add the distances (equality of indices asserted)
 out['distance_km'] = gdf['distance_km']
 
-# step 7: check if there are counties with more off-diagonal trips than inhabitants
+# step 8: check if there are counties with more off-diagonal trips than inhabitants
 n = out[out['origin'] != out['destination']].groupby(by='origin')['commuters'].sum()
 n_test = n.values / demography.values # there are
 
-# step 8: save results
+# step 9: save results
 out = out.drop(columns = ['state_o', 'state_d'])
 out.to_csv(os.path.join(os.getcwd(), f'../interim/mobility/mobility_cellphone_09032020_longform.csv'), index=False)
 
