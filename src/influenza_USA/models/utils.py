4def0abeddd2fea099c3551d55f3b5a2917211b5
Tijs Alleman <tijs.alleman@ugent.be>
2024-08-27 17:42:07 -0400

    some name changes

--- src/influenza_USA/models/utils.py
+++ src/influenza_USA/models/utils.py
@@ -12,91 +12,6 @@ import pandas as pd
 # all paths relative to the location of this file
 abs_dir = os.path.dirname(__file__)
 
-def name2fips(name_state, name_county=None):
-    """
-    A function to convert a US state, or US state and county name into a FIPS code
-    US FIPS codes have the following format: SSCCC 1) The first two digits represent the state, 2) the last three digits represent the county. A state is assigned the county code '000'.
-
-    input:
-    ------
-
-    name_state: str
-        Name of the US state
-    name_county: None/str
-        Name of the US county. (Default: 'None').
-
-    output:
-    -------
-
-    fips: str
-        Five digit US FIPS code. If name_county=None, the fips code of the US state is returned in the format 'xx000'.
-    """
-
-    # load FIPS-name list (fips_state (2 digits), fips_county (3 digits), name_state, name_county)
-    df = pd.read_csv(os.path.join(abs_dir,'../../../data/interim/fips_codes/fips_state_county.csv'), dtype={'fips_state': str, 'fips_county': str})
-
-    # state name only
-    if not isinstance(name_state, str):
-        raise TypeError('provided state name should be a string')
-    if name_state.lower() not in df['name_state'].values:
-        raise ValueError(
-            f"name '{name_state}' is not a valid US state name"
-        )
-    fips_state = df[df['name_state'] == name_state.lower()]['fips_state'].unique()[0]
-    if not name_county:
-        return fips_state + '000'
-    # state and county
-    else:
-        if not isinstance(name_county, str):
-            raise TypeError('provided county name should be a string')
-        if name_county.lower() not in df['name_county'].values:
-            raise ValueError(
-                f"name '{name_county}' is not a valid US county name;\ndon't forget a suffix like 'county'/'parish'/'planning region'"
-            )
-        fips_county = df[((df['name_state'] == name_state.lower()) & (df['name_county'] == name_county.lower()))]['fips_county'].unique()[0]
-        return fips_state + fips_county
-
-def fips2name(fips_code):
-    """
-    A function to convert a five digit US state or US county FIPS code into a name
-    US FIPS codes have the following format: SSCCC 1) The first two digits represent the state, 2) the last three digits represent the county. A state is assigned the county code '000'.
-
-    input:
-    ------
-
-    fips_code: str
-        Five digit FIPS code. 
-
-    output:
-    -------
-
-    name_state: str
-        State name. 
-    
-    name_county: str
-        County name. No county name is returned if the five-digit FIPS code represents a US state (format 'xx000')
-    """
-
-    # check input
-    if not isinstance(fips_code, str):
-        raise TypeError("fips code must be of type 'str'")
-    if len(fips_code) != 5:
-        raise ValueError("fips codes consist of five digits. state fips codes must be extended from 2 to 5 digits with zeros")
-    
-    # split input
-    fips_state = fips_code[0:2]
-    fips_county = fips_code[2:]
-
-    # load FIPS-name list
-    df = pd.read_csv(os.path.join(abs_dir,'../../../data/interim/fips_codes/fips_state_county.csv'), dtype={'fips_state': str, 'fips_county': str})
-
-    # look up name
-    if fips_county == '000':
-        return df[df['fips_state'] == fips_state]['name_state'].unique()[0]
-    else:
-        lu = df[((df['fips_state'] == fips_state) & (df['fips_county'] == fips_county))][['name_state', 'name_county']]
-        return lu['name_state'].values[0], lu['name_county'].values[0]
-    
 def construct_coordinates_dictionary(spatial_resolution='states'):
     """
     A function returning the model's coordinates for the dimension 'age_group' and 'location'. Coordinates derived from the interim demography dataset.
@@ -366,3 +281,89 @@ def construct_initial_infected(seed_loc=('',''), n=1, agedist='demographic', spa
     n_fips = len(demography['fips'].unique())
 
     return np.transpose(I0.values.reshape(n_fips, n_age))
+
+def name2fips(name_state, name_county=None):
+    """
+    A function to convert a US state, or US state and county name into a FIPS code
+    US FIPS codes have the following format: SSCCC 1) The first two digits represent the state, 2) the last three digits represent the county. A state is assigned the county code '000'.
+
+    input:
+    ------
+
+    name_state: str
+        Name of the US state
+    name_county: None/str
+        Name of the US county. (Default: 'None').
+
+    output:
+    -------
+
+    fips: str
+        Five digit US FIPS code. If name_county=None, the fips code of the US state is returned in the format 'xx000'.
+    """
+
+    # load FIPS-name list (fips_state (2 digits), fips_county (3 digits), name_state, name_county)
+    df = pd.read_csv(os.path.join(abs_dir,'../../../data/interim/fips_codes/fips_state_county.csv'), dtype={'fips_state': str, 'fips_county': str})
+
+    # state name only
+    if not isinstance(name_state, str):
+        raise TypeError('provided state name should be a string')
+    if name_state.lower() not in df['name_state'].values:
+        raise ValueError(
+            f"name '{name_state}' is not a valid US state name"
+        )
+    fips_state = df[df['name_state'] == name_state.lower()]['fips_state'].unique()[0]
+    if not name_county:
+        return fips_state + '000'
+    # state and county
+    else:
+        if not isinstance(name_county, str):
+            raise TypeError('provided county name should be a string')
+        if name_county.lower() not in df['name_county'].values:
+            raise ValueError(
+                f"name '{name_county}' is not a valid US county name;\ndon't forget a suffix like 'county'/'parish'/'planning region'"
+            )
+        fips_county = df[((df['name_state'] == name_state.lower()) & (df['name_county'] == name_county.lower()))]['fips_county'].unique()[0]
+        return fips_state + fips_county
+
+def fips2name(fips_code):
+    """
+    A function to convert a five digit US state or US county FIPS code into a name
+    US FIPS codes have the following format: SSCCC 1) The first two digits represent the state, 2) the last three digits represent the county. A state is assigned the county code '000'.
+
+    input:
+    ------
+
+    fips_code: str
+        Five digit FIPS code. 
+
+    output:
+    -------
+
+    name_state: str
+        State name. 
+    
+    name_county: str
+        County name. No county name is returned if the five-digit FIPS code represents a US state (format 'xx000')
+    """
+
+    # check input
+    if not isinstance(fips_code, str):
+        raise TypeError("fips code must be of type 'str'")
+    if len(fips_code) != 5:
+        raise ValueError("fips codes consist of five digits. state fips codes must be extended from 2 to 5 digits with zeros")
+    
+    # split input
+    fips_state = fips_code[0:2]
+    fips_county = fips_code[2:]
+
+    # load FIPS-name list
+    df = pd.read_csv(os.path.join(abs_dir,'../../../data/interim/fips_codes/fips_state_county.csv'), dtype={'fips_state': str, 'fips_county': str})
+
+    # look up name
+    if fips_county == '000':
+        return df[df['fips_state'] == fips_state]['name_state'].unique()[0]
+    else:
+        lu = df[((df['fips_state'] == fips_state) & (df['fips_county'] == fips_county))][['name_state', 'name_county']]
+        return lu['name_state'].values[0], lu['name_county'].values[0]
+    
