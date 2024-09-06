936656325a6b4d650dcd3deb46121b156cac0bf1
Tijs Alleman <tijs.alleman@ugent.be>
2024-09-06 18:00:37 -0400

    calibration script

--- scripts/calibrate_SVI2RHD.py
+++ scripts/calibrate_SVI2RHD.py
@@ -34,8 +34,8 @@ multiplier_pso = 10                                 # PSO swarm size
 processes = int(os.getenv('SLURM_CPUS_ON_NODE', mp.cpu_count())) # Retrieve CPU count
 
 # Bayesian
-identifier = 'normal'                              # Give any output of this script an ID
-n_mcmc = 100                                        # Number of MCMC iterations
+identifier = 'normal_nodaytype'                     # Give any output of this script an ID
+n_mcmc = 200                                        # Number of MCMC iterations
 multiplier_mcmc = 5                                 # Total number of Markov chains = number of parameters * multiplier_mcmc
 print_n = 10                                        # Print diagnostics every print_n iterations
 discard = 20                                        # Discard first `discard` iterations as burn-in
@@ -63,7 +63,7 @@ end_calibration = df.index.max()
 # model settings
 sr = 'states'                       # spatial resolution: 'collapsed', 'states' or 'counties'
 ar = 'full'                         # age resolution: 'collapsed' or 'full'
-dd = True                           # vary contact matrix by daytype
+dd = False                           # vary contact matrix by daytype
 stoch = False                       # ODE vs. tau-leap
 
 # initialise model
@@ -83,16 +83,16 @@ if __name__ == '__main__':
     data=[df['Weekly_Cases'], df['Weekly_Hosp'], df['Weekly_Deaths']]
     weights = [1/max(df['Weekly_Cases']), 1/max(df['Weekly_Hosp']), 1/max(df['Weekly_Deaths'])]
     states = ['I_inc', 'H_inc', 'D_inc']
-    log_likelihood_fnc = [ll_normal, ll_normal, ll_normal]
+    log_likelihood_fnc = [ll_normal, ll_normal, ll_normal] # poisson works about equally well --> just make sure to weigh datasets appropriately!
     log_likelihood_fnc_args = [
         0.033*df['Weekly_Cases'].values+1,
         0.033*df['Weekly_Hosp'].values+1,
         0.033*df['Weekly_Deaths'].values+1
     ]
     # calibated parameters and bounds
-    pars = ['beta', 'rho_h', 'T_d', 'rho_d', 'asc_case']
-    labels = [r'$\beta$', r'$\rho_h$',  r'$T_d$', r'$\rho_d$', r'$\alpha_{case}$']
-    bounds = [(0.01,0.10), (0.001,0.1), (0.1,28), (0.001,1), (0.001,0.1)]
+    pars = ['beta', 'rho_h', 'rho_d', 'asc_case']
+    labels = [r'$\beta$', r'$\rho_h$', r'$\rho_d$', r'$\alpha_{case}$']
+    bounds = [(0.01,0.10), (0.001,0.1), (0.001,1), (0.001,0.1)]
     # Setup objective function (no priors --> uniform priors based on bounds)
     objective_function = log_posterior_probability(model,pars,bounds,data,states,log_likelihood_fnc,log_likelihood_fnc_args,
                                                    start_sim=start_calibration,weights=weights,labels=labels)
@@ -100,7 +100,8 @@ if __name__ == '__main__':
     expanded_labels = objective_function.expanded_labels 
     expanded_bounds = objective_function.expanded_bounds                                   
     # Nelder-mead
-    theta = [0.025, 0.0025, 4.71, 0.05, 0.0015] # no seasonality --> good fit
+    theta = [0.025, 0.0025, 0.05, 0.0015] # With varying datypes --> very good fit
+    theta = [0.024, 0.0025, 0.05, 0.0015] # Without varying datypes --> very good fit
     #step = len(expanded_bounds)*[0.05,]
     #theta = nelder_mead.optimize(objective_function, np.array(theta), step, processes=processes, max_iter=n_pso)[0]
 
@@ -147,7 +148,7 @@ if __name__ == '__main__':
     fig_path='sampler_output/'
     run_date = str(date.today())
     # Perturbate previously obtained estimate
-    ndim, nwalkers, pos = perturbate_theta(theta, pert=0.30*np.ones(len(theta)), multiplier=multiplier_mcmc, bounds=expanded_bounds)
+    ndim, nwalkers, pos = perturbate_theta(theta, pert=0.10*np.ones(len(theta)), multiplier=multiplier_mcmc, bounds=expanded_bounds)
     # Write some usefull settings to a pickle file (no pd.Timestamps or np.arrays allowed!)
     settings={'start_calibration': start_calibration.strftime('%d-%m-%Y'), 'end_calibration': end_calibration.strftime('%d-%m-%Y'),
               'n_chains': nwalkers, 'starting_estimate': list(theta), 'labels': expanded_labels}
@@ -175,7 +176,6 @@ if __name__ == '__main__':
         # Sample model parameters
         idx, parameters['beta'] = random.choice(list(enumerate(samples['beta'])))
         parameters['rho_h'] = samples['rho_h'][idx]
-        parameters['T_d'] = samples['T_d'][idx]
         parameters['rho_d'] = samples['rho_d'][idx]
         parameters['asc_case'] = samples['asc_case'][idx]
         return parameters
