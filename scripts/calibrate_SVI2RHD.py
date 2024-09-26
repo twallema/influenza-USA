cc92e4f9d9e6f144ebbae265e966bb4def906ed3
Tijs Alleman <tijs.alleman@ugent.be>
2024-09-26 15:17:42 -0400

    calibration of f_I and f_R

--- scripts/calibrate_SVI2RHD.py
+++ scripts/calibrate_SVI2RHD.py
@@ -34,11 +34,11 @@ stoch = False                       # ODE vs. tau-leap
 
 # optimization
 ## frequentist
-n_pso = 100                                                         # Number of PSO iterations
+n_pso = 50                                                         # Number of PSO iterations
 multiplier_pso = 10                                                 # PSO swarm size
 ## bayesian
 identifier = waning                                                 # Use waning as identifier of script output
-n_mcmc = 500                                                        # Number of MCMC iterations
+n_mcmc = 1000                                                        # Number of MCMC iterations
 multiplier_mcmc = 10                                                # Total number of Markov chains = number of parameters * multiplier_mcmc
 print_n = 10                                                        # Print diagnostics every `print_n`` iterations
 discard = 100                                                       # Discard first `discard` iterations as burn-in
@@ -72,6 +72,7 @@ df.index.rename('date', inplace=True)
 # slice data until end
 df = df.loc[slice(None, end_calibration)]
 df_peak = df.loc[slice(start_peakslice, end_peakslice)]
+df_bump = df.loc[slice(datetime(2017, 8, 1), datetime(2017, 8, 16))]
 # replace `end_calibration` None --> datetime
 end_calibration = df.index.max()
 
@@ -92,9 +93,7 @@ elif waning == 'waning_180':
     model.parameters['T_v'] = 365/2
 
 # set up a draw function with calibratable infected and recovered fractions
-def draw_function_calibration(parameters, initial_states, f_R):
-
-    f_I = 1e-4
+def draw_function_calibration(parameters, initial_states, f_I, f_R):
 
     # What I'd like to calibrate
     initial_states['S'] = (1-f_I-f_R) * construct_initial_susceptible(sr, ar)
@@ -121,19 +120,21 @@ if __name__ == '__main__':
 
     # define datasets
     data=[df['Weekly_Cases'], df['Weekly_Hosp'], df['Weekly_Deaths'],                                                                                                                       # all data
-          df_peak['Weekly_Hosp'], df_peak['Weekly_Deaths']]   # hospital/death peak counted double
+          df_peak['Weekly_Hosp'], df_peak['Weekly_Deaths'],
+          df_bump['Weekly_Hosp'], df_bump['Weekly_Deaths']]   # hospital/death peak counted double
     # use maximum value in dataset as weight
     weights = [1/max(df['Weekly_Cases']), 1/max(df['Weekly_Hosp']), 1/max(df['Weekly_Deaths']),
-               5/max(df['Weekly_Hosp']), 5/max(df['Weekly_Deaths'])]
+               5/max(df['Weekly_Hosp']), 5/max(df['Weekly_Deaths']),
+               100/max(df['Weekly_Hosp']), 100/max(df['Weekly_Deaths'])]
     # states to match with datasets
-    states = ['I_inc', 'H_inc', 'D_inc', 'H_inc', 'D_inc']
+    states = ['I_inc', 'H_inc', 'D_inc', 'H_inc', 'D_inc', 'H_inc', 'D_inc']
     # log likelihood function + arguments
-    log_likelihood_fnc = [ll_poisson, ll_poisson, ll_poisson, ll_poisson, ll_poisson] 
-    log_likelihood_fnc_args = [[],[],[],[],[]]
+    log_likelihood_fnc = [ll_poisson, ll_poisson, ll_poisson, ll_poisson, ll_poisson, ll_poisson, ll_poisson] 
+    log_likelihood_fnc_args = [[],[],[],[],[],[],[]]
     # parameters to calibrate and bounds
-    pars = ['beta', 'rho_h', 'rho_d', 'asc_case', 'f_R']
-    labels = [r'$\beta$', r'$\rho_h$', r'$\rho_d$', r'$\alpha_{case}$', r'$f_R$']
-    bounds = [(0.001,0.05), (0.001,0.1), (0.001,1), (0.001,0.1), (0,1)]
+    pars = ['beta', 'rho_h', 'rho_d', 'asc_case', 'f_I', 'f_R']
+    labels = [r'$\beta$', r'$\rho_h$', r'$\rho_d$', r'$\alpha_{case}$', r'$f_I$', r'$f_R$']
+    bounds = [(0.001,0.048), (0.001,0.1), (0.001,1), (0.001,0.1), (1e-8,1), (0,1)]
     # Setup objective function (no priors defined = uniform priors based on bounds)
     objective_function = log_posterior_probability(model,pars,bounds,data,states,log_likelihood_fnc,log_likelihood_fnc_args,draw_function=draw_function_calibration,
                                                    start_sim=start_calibration, weights=weights, labels=labels)
@@ -144,23 +145,24 @@ if __name__ == '__main__':
 
     # Initial guess
     # season: 2017-2018
-    theta = [0.03216971, 0.00315444, 0.04745116, 0.00274503, 0.63861876] # --> no waning
+    theta = [2.97959440e-02, 3.33875610e-03, 4.77082218e-02, 2.74330510e-03, 9.13140716e-05, 5.83462036e-01] # --> no waning
+    #theta = [0.03081685, 0.0031967, 0.04753118, 0.00297124, 0.605574] # --> waning 180 d
     # season: 2019-2020
     #theta = [0.0242, 0.004, 0.03, 0.0030] # --> no vaccine waning waning
     #theta = [0.024, 0.003, 0.03, 0.0025] # --> efficacy 80% at start, vaccine waning at rate of 180 days
 
     # Perform optimization 
-    step = len(bounds)*[0.05,]
-    theta = nelder_mead.optimize(objective_function, np.array(theta), step, processes=processes, max_iter=n_pso)[0]
+    #step = len(bounds)*[0.05,]
+    #theta = nelder_mead.optimize(objective_function, np.array(theta), step, processes=processes, max_iter=n_pso)[0]
 
     ######################
     ## Visualize result ##
     ######################
 
     # Assign results to model
-    model.parameters = assign_theta(model.parameters, pars[:-1], theta[:-1])
+    model.parameters = assign_theta(model.parameters, pars[:-2], theta[:-2])
     # Simulate model
-    out = model.sim([start_calibration, end_calibration], N=1, draw_function=draw_function_calibration, draw_function_kwargs={'f_R': theta[-1]})
+    out = model.sim([start_calibration, end_calibration], N=1, draw_function=draw_function_calibration, draw_function_kwargs={'f_I': theta[-2], 'f_R': theta[-1]})
     # Add poisson obervational noise
     #out = add_poisson_noise(out)
     # Visualize
@@ -194,7 +196,7 @@ if __name__ == '__main__':
     # Variables
     samples_path=fig_path=f'../data/interim/calibration/{season}/{identifier}/'
     # Perturbate previously obtained estimate
-    ndim, nwalkers, pos = perturbate_theta(theta, pert=[0.2, 0.2, 0.2, 0.2, 2], multiplier=multiplier_mcmc, bounds=bounds)
+    ndim, nwalkers, pos = perturbate_theta(theta, pert=[0.2, 0.2, 0.2, 0.2, 0.99, 0.99], multiplier=multiplier_mcmc, bounds=bounds)
     # Append some usefull settings to the samples dictionary
     settings={'start_calibration': start_calibration.strftime('%Y-%m-%d'), 'end_calibration': end_calibration.strftime('%Y-%m-%d'),
               'n_chains': nwalkers, 'starting_estimate': list(theta), 'labels': labels, 'season': season,
@@ -212,7 +214,7 @@ if __name__ == '__main__':
     for idx,ax in enumerate(fig.get_axes()):
         ax.grid(False)
     plt.savefig(fig_path+'corner.pdf')
-    plt.show()
+    #plt.show()
     plt.close()
 
     ######################
@@ -228,7 +230,7 @@ if __name__ == '__main__':
         parameters['asc_case'] = samples['asc_case'][idx]
 
         # sample initial condition
-        f_I = 1e-4
+        f_I = samples['f_I'][idx]
         f_R = samples['f_R'][idx]
         initial_states['S'] = (1-f_I-f_R) * construct_initial_susceptible(sr, ar)
         initial_states['I'] = f_I * construct_initial_susceptible(sr, ar)
