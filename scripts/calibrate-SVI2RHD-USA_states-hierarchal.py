86915dc741e56643ddba1f7d1c46b9699d949573
Tijs Alleman <tijs.alleman@ugent.be>
2024-11-19 19:37:29 -0500

    calibration script

--- scripts/calibrate-SVI2RHD-USA_states-hierarchal.py
+++ scripts/calibrate-SVI2RHD-USA_states-hierarchal.py
@@ -27,8 +27,8 @@ from pySODM.optimization.mcmc import perturbate_theta, run_EnsembleSampler, emce
 ##############
 
 # model settings
-season_start = 2017                     # '2017' or '2019'
-season = '2017-2018'                    # '2017-2018' or '2019-2020'
+season_start = 2014                     # '2017' or '2019'
+season = '2014-2015'                    # '2017-2018' or '2019-2020'
 waning = 'no_waning'                    # 'no_waning' vs. 'waning_180'
 sr = 'states'                           # spatial resolution: 'collapsed', 'states' or 'counties'
 ar = 'full'                             # age resolution: 'collapsed' or 'full'
@@ -38,60 +38,60 @@ hierarchal_transmission_rate = True     # Hierarchal structure on transmission r
 hierarchal_immunity = True              # Hierarchal structure on the waning of the natural immunity
 
 # optimization
-start_calibration = datetime(season_start, 10, 15)                               # simulations will start on this date
-end_calibration = datetime(season_start+1, 2, 15)                                # 2017-2018: None, 2019-2020: datetime(2020,3,22) - exclude COVID
-start_slice = datetime(season_start+1, 1, 1)                                     # add in a part of the dataset twice: in this case the peak in hosp.
+start_calibration = datetime(season_start, 10, 15)                              # simulations will start on this date
+end_calibration = datetime(season_start+1, 5, 2)                                # 2017-2018: None, 2019-2020: datetime(2020,3,22) - exclude COVID
+start_slice = datetime(season_start+1, 1, 1)                                    # add in a part of the dataset twice: in this case the peak in hosp.
 end_slice = datetime(season_start+1, 3, 1)
 ## frequentist
-n_pso = 500                                                                     # Number of PSO iterations
+n_pso = 5000                                                                    # Number of PSO iterations
 multiplier_pso = 10                                                             # PSO swarm size
 ## bayesian
-identifier = 'USA_regions_hierarchal_midFeb-waning'                         # ID of run
+identifier = 'USA_regions_hierarchal_May-waning'                                # ID of run
 samples_path=fig_path=f'../data/interim/calibration/{season}/{identifier}/'     # Path to backend
-n_mcmc = 1                                                                 # Number of MCMC iterations
+n_mcmc = 5000                                                                   # Number of MCMC iterations
 multiplier_mcmc = 3                                                             # Total number of Markov chains = number of parameters * multiplier_mcmc
-print_n = 1000                                                                   # Print diagnostics every `print_n`` iterations
-discard = 4000                                                                     # Discard first `discard` iterations as burn-in
-thin = 100                                                                         # Thinning factor emcee chains
+print_n = 1000                                                                  # Print diagnostics every `print_n`` iterations
+discard = 0                                                                     # Discard first `discard` iterations as burn-in
+thin = 100                                                                      # Thinning factor emcee chains
 n = 500                                                                         # Repeated simulations used in visualisations
-processes = 16                                                                   # Retrieve CPU count
+processes = 16                                                                  # Retrieve CPU count
 ## hierarchal hyperparameters                                                       
-L1_weight = 5
+L1_weight = 2
 rel_weight_level2 = 2
 n_regions = 9
 n_states = 52
 n_temporal_modifiers = 10
 
 ## continue run
-run_date = '2024-10-22'                                                         # First date of run
-backend_identifier = 'USA_regions_hierarchal_midFeb-waning'
-backend_path = f"../data/interim/calibration/{season}/{backend_identifier}/{backend_identifier}_BACKEND_{run_date}.hdf5"
+# run_date = '2024-10-30'                                                         # First date of run
+# backend_identifier = 'USA_regions_hierarchal_May-waning'
+# backend_path = f"../data/interim/calibration/{season}/{backend_identifier}/{backend_identifier}_BACKEND_{run_date}.hdf5"
 ## new run
-# backend_path = None
-# if not backend_path:
-#    run_date = datetime.today().strftime("%Y-%m-%d")
-# # national estimates
-# beta_US = 0.0325
-# delta_beta_regions = 0.01
-# delta_beta_states = 0.01
-# delta_beta_temporal = 0.01
-# delta_beta_regions_Nov1 = 0.01
-# delta_beta_regions_Nov2 = 0.01
-# delta_beta_regions_Dec1 = 0.01
-# delta_beta_regions_Dec2 = 0.01
-# delta_beta_regions_Jan1 = 0.01
-# delta_beta_regions_Jan2 = 0.01
-# delta_beta_regions_Feb1 = 0.01
-# delta_beta_regions_Feb2 = 0.01
-# delta_beta_regions_Mar1 = 0.01
-# delta_beta_regions_Mar2 = 0.01
-# f_R = 0.50
-# delta_f_R_states = 0.01
-# delta_f_R_regions = 0.01
-# T_r_US = 365/np.log(2)
-# delta_T_r_regions = 0.01
-# rho_h = 0.00334
-# f_I = 2e-4
+backend_path = None
+if not backend_path:
+   run_date = datetime.today().strftime("%Y-%m-%d")
+# national estimates
+beta_US = 0.035
+delta_beta_regions = 0.01
+delta_beta_states = 0.01
+delta_beta_temporal = 0.01
+delta_beta_regions_Nov1 = 0.01
+delta_beta_regions_Nov2 = 0.01
+delta_beta_regions_Dec1 = 0.01
+delta_beta_regions_Dec2 = 0.01
+delta_beta_regions_Jan1 = 0.01
+delta_beta_regions_Jan2 = 0.01
+delta_beta_regions_Feb1 = 0.01
+delta_beta_regions_Feb2 = 0.01
+delta_beta_regions_Mar1 = 0.01
+delta_beta_regions_Mar2 = 0.01
+f_R = 0.50
+delta_f_R_states = 0.01
+delta_f_R_regions = 0.01
+T_r_US = 365/np.log(2)
+delta_T_r_regions = 0.01
+rho_h = 0.0026
+f_I = 2e-4
 
 ###############################
 ## Load hospitalisation data ##
@@ -115,6 +115,9 @@ end_calibration = df_calibration.index.get_level_values('date').unique().max() +
 # now slice out the remainder of the dataset
 df_validation = df.loc[slice(end_calibration, None), slice(None)]
 
+print(df_calibration)
+print(df_validation)
+
 #####################################################
 ## Load previous sampler and extract last estimate ##
 #####################################################
@@ -127,12 +130,6 @@ if backend_path:
     pos = backend.get_chain(discard=discard, thin=thin, flat=False)[-1, ...]
     # Average out all walkers/parameter
     theta = np.mean(pos, axis=0)
-
-# >>>>>>>>>>>>>>>>>>>>>>>>>
-# temporary code
-#theta = list(theta)
-#theta = theta[0:4] + [365/np.log(2),] + theta[4:4+2*n_regions+n_temporal_modifiers] + list(1e-2*np.ones(n_regions)) + theta[4+2*n_regions+n_temporal_modifiers:]
-# >>>>>>>>>>>>>>>>>>>>>>>>>
 
 # Function to get indices of a states fips
 def get_pos_beta_f_R(fips, model_coordinates):
@@ -203,7 +200,7 @@ if __name__ == '__main__':
                 r'$\Delta \beta_{regions, Mar 1}$', r'$\Delta \beta_{regions, Mar 2}$',
                 ]
     # parameter bounds
-    bounds = [(1e-9,0.01), (1e-8,1e-3), (0.01,0.060), (0.10,0.90), (365/2/np.log(2), 2000),
+    bounds = [(1e-9,0.01), (1e-8,1e-3), (0.01,0.060), (0.10,0.90), ((365*8/12)/np.log(2), 2000),
               (-0.5,0.5), (-0.5,0.5), (-0.5,0.5), (-0.5,0.5),
               (-0.5,0.5), (-0.5,0.5),
               (-0.5,0.5), (-0.5,0.5), 
@@ -226,7 +223,7 @@ if __name__ == '__main__':
     stdev = 0.10
     log_prior_prob_fcn_args = [
         # top level
-        bounds[0], bounds[1], bounds[2], bounds[3], (2, np.log(2)/365*2, 365/2/np.log(2)),
+        bounds[0], bounds[1], bounds[2], bounds[3], (12/8, np.log(2)/(365*8/12), (365*8/12)/np.log(2)),
         # mid level
         (0, stdev,  L1_weight), (0, stdev,  L1_weight), (0, stdev,  L1_weight), (0, stdev,  L1_weight),
         # bottom level
@@ -248,7 +245,7 @@ if __name__ == '__main__':
 
     # Initial guess
     if not backend_path:
-        theta = [rho_h, f_I, beta_US, f_R, T_r] + n_regions*[delta_beta_regions,] + n_temporal_modifiers*[delta_beta_temporal,] + \
+        theta = [rho_h, f_I, beta_US, f_R, T_r_US] + n_regions*[delta_beta_regions,] + n_temporal_modifiers*[delta_beta_temporal,] + \
                     n_regions*[delta_f_R_states,] + n_regions * [delta_T_r_regions,] + \
                         n_states * [delta_beta_states,] + n_states * [delta_f_R_states,] + \
                             n_regions*[delta_beta_regions_Nov1,] + n_regions*[delta_beta_regions_Nov2,] + \
@@ -277,20 +274,19 @@ if __name__ == '__main__':
     x_calibration_data = df_calibration.index.get_level_values('date').unique().values
     x_validation_data = df_validation.index.get_level_values('date').unique().values
     ax[0].scatter(x_calibration_data, 7*df_calibration.groupby(by='date').sum(), color='black', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
-    ax[0].scatter(x_validation_data, 7*df_validation.groupby(by='date').sum(), color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
+    if not df_validation.empty:
+        ax[0].scatter(x_validation_data, 7*df_validation.groupby(by='date').sum(), color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
     ax[0].plot(out['date'], 7*out['H_inc'].sum(dim=['age_group', 'location']), color='blue', alpha=1, linewidth=2)
-    #ax[0].set_title(f'$\\beta = {theta[2]:.3f}$ (USA)\n$\\Delta \\beta (Dec)={100*theta[4]:.1f}$%, $\\Delta \\beta (Jan)={100*theta[5]:.1f}$%, $\\Delta \\beta (Feb)={100*theta[6]:.1f}$%, $\\Delta \\beta (Mar)={100*theta[7]:.1f}$%')
     ax[0].grid(False)
 
     ## per state
     for i,loc in enumerate(df.index.get_level_values('location').unique().values):
         ax[i+1].scatter(x_calibration_data, 7*df_calibration.loc[slice(None), loc], color='black', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
-        ax[i+1].scatter(x_validation_data, 7*df_validation.loc[slice(None), loc], color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
+        if not df_validation.empty:
+            ax[i+1].scatter(x_validation_data, 7*df_validation.loc[slice(None), loc], color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
         ax[i+1].plot(out['date'], 7*out['H_inc'].sum(dim=['age_group']).sel(location=loc), color='blue', alpha=1, linewidth=2)
         ax[i+1].set_title(f"{fips2name(loc)} ({loc})")
         pos_beta, pos_f_R = get_pos_beta_f_R(loc, model.coordinates['location'])
-        #ax[i+1].text(0.05, 0.95, f"$\\Delta \\beta_i$: {100*theta[pos_beta]:.1f}%, $f_R$: {theta[pos_f_R]:.2f}", transform=ax[i+1].transAxes, fontsize=12,
-        #    verticalalignment='top', bbox=props)
         ax[i+1].grid(False)
 
     ## format dates
@@ -370,24 +366,23 @@ if __name__ == '__main__':
     x_calibration_data = df_calibration.index.get_level_values('date').unique().values
     x_validation_data = df_validation.index.get_level_values('date').unique().values
     ax[0].scatter(x_calibration_data, 7*df_calibration.groupby(by='date').sum(), color='black', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
-    ax[0].scatter(x_validation_data, 7*df_validation.groupby(by='date').sum(), color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
+    if not df_validation.empty:
+        ax[0].scatter(x_validation_data, 7*df_validation.groupby(by='date').sum(), color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
     ax[0].plot(out['date'], 7*out['H_inc'].sum(dim=['age_group', 'location']).mean(dim='draws'), color='blue', alpha=1, linewidth=2)
-    #ax[0].fill_between(out['date'], 7*out['H_inc'].sum(dim=['age_group', 'location']).quantile(dim='draws', q=0.05/2),
-    #                    7*out['H_inc'].sum(dim=['age_group', 'location']).quantile(dim='draws', q=1-0.05/2), color='blue', alpha=0.2)
-    ax[0].set_title(f'$\\beta = {theta[2]:.3f}$ (USA)\n$\\Delta \\beta (Dec)={100*theta[4]:.1f}$%, $\\Delta \\beta (Jan)={100*theta[5]:.1f}$%, $\\Delta \\beta (Feb)={100*theta[6]:.1f}$%, $\\Delta \\beta (Mar)={100*theta[7]:.1f}$%')
+    ax[0].fill_between(out['date'], 7*out['H_inc'].sum(dim=['age_group', 'location']).quantile(dim='draws', q=0.05/2),
+                        7*out['H_inc'].sum(dim=['age_group', 'location']).quantile(dim='draws', q=1-0.05/2), color='blue', alpha=0.2)
     ax[0].grid(False)
 
     ## per state
     for i,loc in enumerate(df.index.get_level_values('location').unique().values):
         ax[i+1].scatter(x_calibration_data, 7*df_calibration.loc[slice(None), loc], color='black', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
-        ax[i+1].scatter(x_validation_data, 7*df_validation.loc[slice(None), loc], color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
+        if not df_validation.empty:
+            ax[i+1].scatter(x_validation_data, 7*df_validation.loc[slice(None), loc], color='red', alpha=1, linestyle='None', facecolors='None', s=60, linewidth=2)
         ax[i+1].plot(out['date'], 7*out['H_inc'].sum(dim=['age_group']).sel(location=loc).mean(dim='draws'), color='blue', alpha=1, linewidth=2)
         ax[i+1].fill_between(out['date'], 7*out['H_inc'].sum(dim=['age_group']).sel(location=loc).quantile(dim='draws', q=0.05/2),
                              7*out['H_inc'].sum(dim=['age_group']).sel(location=loc).quantile(dim='draws', q=1-0.05/2), color='blue', alpha=0.2)
         ax[i+1].set_title(f"{fips2name(loc)} ({loc})")
         pos_beta, pos_f_R = get_pos_beta_f_R(loc, model.coordinates['location'])
-        #ax[i+1].text(0.05, 0.95, f"$\\Delta \\beta_i$: {100*theta[pos_beta]:.1f}%, $f_R$: {theta[pos_f_R]:.2f}", transform=ax[i+1].transAxes, fontsize=12,
-        #    verticalalignment='top', bbox=props)
         ax[i+1].grid(False)
 
     ## format dates
