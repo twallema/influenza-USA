c3aa57f68fb77697c03866430fecb2c6efea0475
Tijs Alleman <tijs.alleman@ugent.be>
2024-11-19 19:06:39 -0500

    speed model up

--- src/influenza_USA/SVIR/model.py
+++ src/influenza_USA/SVIR/model.py
@@ -32,11 +32,23 @@ class ODE_SVI2RHD(ODE):
         T = S+V+I+Iv+R+H
 
         # compute visiting populations
+        S_v = np.matmul(S, M)
+        V_v = np.matmul(V, M)
         I_v = np.matmul((I+Iv), M)
         T_v = np.matmul(T, M)
 
-        # compute force of infection
-        l = np.atleast_1d(beta)[np.newaxis, :] * (tf.einsum ('lj, il -> ij', (I+Iv)/T, (1-f_v)*N) + tf.einsum ('jk, lk, il -> ij', M, I_v/T_v, f_v*N))
+        # compute force of infection (elegant einsum implementation, slower)
+        # l = np.atleast_1d(beta)[np.newaxis, :] * (tf.einsum ('il, lg -> ig', (1-f_v)*N, (I+Iv)/T) + tf.einsum ('gk, il, lk -> ig', M, f_v*N, I_v/T_v))
+
+        # compute new infections (long numpy implementation, faster)
+        # compute number of new infections on residency and workplace patches
+        dI_residency_unvacc = beta * S * np.matmul((1-f_v)*N, (I+Iv)/T)
+        dI_work_unvacc = beta * S_v * np.matmul ((1-f_v)*N, I_v/T_v)
+        dI_residency_vacc = beta * (1-e_i) * V * np.matmul((1-f_v)*N, (I+Iv)/T)
+        dI_work_vacc = beta * (1-e_i) * V_v * np.matmul ((1-f_v)*N, I_v/T_v)
+        # redistribute the new infections on workplace patches back to residency patch
+        dI_unvacc = dI_residency_unvacc + S * np.transpose(np.atleast_2d(M) @ np.transpose(dI_work_unvacc/S_v))
+        dI_vacc = dI_residency_vacc + V * np.transpose(np.atleast_2d(M) @ np.transpose(dI_work_vacc/V_v))
 
         # u-shaped severity curve
         rho_h = (rho_h * CHR)[:, np.newaxis]
@@ -45,16 +57,16 @@ class ODE_SVI2RHD(ODE):
         n_vacc = n_vacc * (S/(T-V-I-Iv))
 
         # calculate state differentials
-        dS = - n_vacc - l*S + (1/T_r)*R + (1/T_v)*V
-        dV = n_vacc - (1-e_i)*l*V  - (1/T_v)*V
-        dI = l*S - (1/T_h)*I
-        dIv = (1-e_i)*l*V - (1/T_h)*Iv
+        dS = - n_vacc - dI_unvacc + (1/T_r)*R + (1/T_v)*V
+        dV = n_vacc - dI_vacc - (1/T_v)*V
+        dI = dI_unvacc - (1/T_h)*I
+        dIv = dI_vacc - (1/T_h)*Iv
         dR = (1-rho_h)*(1/T_h)*I + 1 - ((1-e_h)*rho_h)*(1/T_h)*Iv + (1-rho_d)*(1/T_d)*H - (1/T_r)*R
         dH = rho_h*(1/T_h)*I + rho_h*(1-e_h)*(1/T_h)*Iv - (1/T_d)*H
         dD = rho_d*(1/T_d)*H
 
         # calculate outcome differentials
-        dI_inc = asc_case*(l*S + (1-e_i)*l*V) - I_inc
+        dI_inc = asc_case*(dI_unvacc + dI_vacc) - I_inc
         dH_inc = rho_h*(1/T_h)*I + rho_h*(1-e_h)*(1/T_h)*Iv - H_inc
         dD_inc = dD - D_inc
 
