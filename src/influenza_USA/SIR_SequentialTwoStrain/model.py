36e0005e5275bc1909a105d5ad167ce89c4a589d
Tijs Alleman <tijs.alleman@ugent.be>
2025-01-14 09:44:23 -0500

    model

--- src/influenza_USA/SIR_SequentialTwoStrain/model.py
+++ src/influenza_USA/SIR_SequentialTwoStrain/model.py
@@ -18,14 +18,14 @@ class ODE_SIR_SequentialTwoStrain(ODE):
     Influenza model with vaccines and age/spatial stratification
     """
     
-    states = ['S','I1','I2','R1','R2','I12','I21','R',      # states
-              'I1_inc', 'I2_inc',                           # needed for outcomes
+    states = ['S','I1','I2','R1','R2','I12','I21','R',                  # states
+              'I_inc', 'H1_inc', 'H2_inc', 'H1_inc_0', 'H2_inc_0',      # outcomes
               ]
     parameters = ['beta1', 'beta2', 'N', 'T_r', 'T_h', 'rho_i', 'rho_h1', 'rho_h2', 'CHR']
     dimensions = ['age_group', 'location']
 
     @staticmethod
-    def integrate(t, S, I1, I2, R1, R2, I12, I21, R, I1_inc, I2_inc, beta1, beta2, N, T_r, T_h, rho_i, rho_h1, rho_h2, CHR):
+    def integrate(t, S, I1, I2, R1, R2, I12, I21, R, I_inc, H1_inc, H2_inc, H1_inc_0, H2_inc_0, beta1, beta2, N, T_r, T_h, rho_i, rho_h1, rho_h2, CHR):
 
         # compute total population
         T = S+I1+I2+R1+R2+I12+I21+R
@@ -42,6 +42,11 @@ class ODE_SIR_SequentialTwoStrain(ODE):
         I2_new = beta2 * S * np.matmul(N, infpop_2)
         I12_new = beta2 * R1 * np.matmul(N, infpop_2)
 
+        # U-shaped severity curve
+        rho_i = (rho_i * CHR)[:, np.newaxis]
+        rho_h1 = (rho_h1 * CHR)[:, np.newaxis]
+        rho_h2 = (rho_h2 * CHR)[:, np.newaxis]
+
         # calculate state differentials
         dS = - (I1_new + I2_new)
         dI1 = I1_new - (1/T_r) * I1
@@ -52,28 +57,15 @@ class ODE_SIR_SequentialTwoStrain(ODE):
         dI21 = I21_new - (1/T_r) * I21
         dR = (1/T_r) * (I12 + I21)
 
-        # calculate outcome differentials
-        dI1_inc = (I1_new + I21_new) - I1_inc
-        dI2_inc = (I2_new + I12_new) - I2_inc
+        # calculate outcome differentials 
+        dI_inc = (I1_new + I21_new + I2_new + I12_new)*rho_i - I_inc
 
-        return dS, dI1, dI2, dR1, dR2, dI12, dI21, dR, dI1_inc, dI2_inc
-
-
-    # TODO: add to model states + checks
-    outcomes = ['I_inc_obs', 'H1_inc', 'H2_inc']
-
-    @staticmethod
-    def compute_outcomes(simout, beta1, beta2, N, T_r, T_h, rho_i, rho_h1, rho_h2, CHR):
-        
-        # U-shaped severity curve
-        rho_i = (rho_i * CHR)[:, np.newaxis]
-        rho_h1 = (rho_h1 * CHR)[:, np.newaxis]
-        rho_h2 = (rho_h2 * CHR)[:, np.newaxis]
-
-        # Infectious (I) observed incidence
-        simout['I_inc_obs'] = (simout['I1_inc'] + simout['I2_inc'])*rho_i
-        # Hospitalised (H) observed incidence
-        simout['H1_inc'] = (simout['I1_inc']*rho_h1).shift(date=T_h, fill_value=0)
-        simout['H2_inc'] = (simout['I2_inc']*rho_h2).shift(date=T_h, fill_value=0)
-
-        return simout
+        # delayed hospitalisation -- exponentially distributed delay
+        ## strain I
+        dH1_inc_0 = (I1_new + I21_new)*rho_h1 - (1/T_h)*H1_inc_0
+        dH1_inc = (1/T_h)*H1_inc_0 - H1_inc
+        # strain II
+        dH2_inc_0 = (I2_new + I12_new)*rho_h2 - (1/T_h)*H2_inc_0
+        dH2_inc = (1/T_h)*H2_inc_0 - H2_inc
+ 
+        return dS, dI1, dI2, dR1, dR2, dI12, dI21, dR, dI_inc, dH1_inc, dH2_inc, dH1_inc_0, dH2_inc_0
