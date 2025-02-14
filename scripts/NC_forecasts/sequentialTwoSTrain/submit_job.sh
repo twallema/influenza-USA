b76a92cc7309d825348a201bc55a3308b23b63f3
Tijs Alleman <tijs.alleman@ugent.be>
2025-02-14 16:30:16 -0500

    batch submission scripts rockfish

---
+++ scripts/NC_forecasts/sequentialTwoSTrain/submit_job.sh
@@ -0,0 +1,21 @@
+#!/bin/bash
+#SBATCH --job-name=hierarchical_training
+#SBATCH --nodes=1
+#SBATCH --ntasks=1
+#SBATCH --cpus-per-task=48
+#SBATCH --time=72:00:00
+
+# Pin the number of cores for use in python calibration script
+export NUM_CORES=$SLURM_CPUS_PER_TASK
+
+# Load any necessary modules or dependencies
+module load anaconda3
+
+# Activate the virtual environment
+conda activate INFLUENZA-USA
+
+# Run your Python script
+python hierarchical_training-SIR_sequentialTwoStrain.py
+
+# Deactivate the virtual environment
+conda deactivate
