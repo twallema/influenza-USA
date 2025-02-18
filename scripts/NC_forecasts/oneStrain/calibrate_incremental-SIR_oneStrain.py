819646a7877557e584850abb3aa146638be7efdf
Tijs Alleman <tijs.alleman@ugent.be>
2025-02-18 18:21:18 -0500

    tryout argumentparser

--- scripts/NC_forecasts/oneStrain/calibrate_incremental-SIR_oneStrain.py
+++ scripts/NC_forecasts/oneStrain/calibrate_incremental-SIR_oneStrain.py
@@ -22,13 +22,36 @@ from pySODM.optimization.utils import assign_theta, add_poisson_noise
 from pySODM.optimization.objective_functions import log_posterior_probability, ll_poisson, log_prior_normal, log_prior_uniform, log_prior_gamma, log_prior_normal, log_prior_beta
 from pySODM.optimization.mcmc import perturbate_theta, run_EnsembleSampler
 
+#####################
+## Parse arguments ##
+#####################
+
+import argparse
+# helper function
+def str_to_bool(value):
+    """Convert string arguments to boolean (for SLURM environment variables)."""
+    return value.lower() in ["true", "1", "yes"]
+
+# parse arguments
+parser = argparse.ArgumentParser()
+parser.add_argument("--use_ED_visits", type=str_to_bool)
+parser.add_argument("--informed", type=str_to_bool)
+parser.add_argument("--season", type=str)
+args = parser.parse_args()
+
+# assign to desired variables
+use_ED_visits = args.use_ED_visits
+informed = args.informed
+season = args.season
+print(use_ED_visits, informed, season)
+
 ##############
 ## Settings ##
 ##############
 
 # model settings
 state = 'North Carolina'                            # state we'd like to calibrate to
-season = '2014-2015'                                # season to calibrate
+#season = '2014-2015'                                # season to calibrate
 sr = 'states'                                       # spatial resolution: 'states' or 'counties'
 ar = 'full'                                         # age resolution: 'collapsed' or 'full'
 dd = False                                          # vary contact matrix by daytype
@@ -38,8 +61,8 @@ L1_weight = 1                                       # Forcing strength on tempor
 stdev = 0.10                                        # Expected standard deviation on temporal modifiers
 
 # optimization parameters
-informed = True                                             # use priors informed by previous seasons
-use_ED_visits = True                                        # use both ED admission (hospitalisation) and ED visits (ILI) data 
+#informed = True                                             # use priors informed by previous seasons
+#use_ED_visits = True                                        # use both ED admission (hospitalisation) and ED visits (ILI) data 
 ## dates
 start_calibration = datetime(season_start, 12, 1)           # incremental calibration will start from here
 end_calibration = datetime(season_start+1, 4, 7)            # and incrementally (weekly) calibrate until this date
--- scripts/NC_forecasts/oneStrain/submit_job.sh
+++ scripts/NC_forecasts/oneStrain/submit_job.sh
@@ -2,8 +2,8 @@
 #SBATCH --job-name=hierarchical_training
 #SBATCH --nodes=1
 #SBATCH --ntasks=1
-#SBATCH --cpus-per-task=24
-#SBATCH --time=48:00:00
+#SBATCH --cpus-per-task=2
+#SBATCH --time=1:00:00
 
 # Pin the number of cores for use in python calibration script
 export NUM_CORES=$SLURM_CPUS_PER_TASK
@@ -15,7 +15,7 @@ module load anaconda3
 conda activate INFLUENZA-USA
 
 # Run your Python script
-python calibrate_incremental-SIR_oneStrain.py
+python calibrate_incremental-SIR_oneStrain.py --use_ED_visits "$USE_ED_VISITS" --informed "$INFORMED" --season "$SEASON"
 
 # Deactivate the virtual environment
 conda deactivate
